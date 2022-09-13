bdbf318089b66e97ab99f1b21dc424748c5c3e76
Doreen Tenai <dntenai@gmail.com>
2022-09-12 19:11:29 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Printspossible combination of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, p;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (p = 0; p < 100; i++)
+		{
+			if (p > i)
+			{
+				putchar((i / 10) + '0');
+			        putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((p / 10) + '0');
+				putchar((p % 10) + '0');
+				if (i != 98)
+				{
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
