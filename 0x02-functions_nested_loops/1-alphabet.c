5451e21659015a5db6d1de06ecfae4ca24be910f
Doreen Tenai <dntenai@gmail.com>
2022-09-14 14:53:06 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * print_alphabet - check the code for Holberton School students.
  * Description: Prints the alphabet with _putchar
  * Return: Always 0.
- * /
+ */
 void print_alphabet(void)
 {
 		char c;
