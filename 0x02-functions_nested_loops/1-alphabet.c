42f0c17547095fade751b3e41e2898e25522ae80
Doreen Tenai <dntenai@gmail.com>
2022-09-15 07:03:16 -0700

    Multiple mains

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,15 @@
 #include "main.h"
-
 /**
- * print_alphabet - check the code for Holberton School students.
- * Description: Prints the alphabet with _putchar
- * Return: Always 0.
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
  */
 void print_alphabet(void)
 {
-		char c;
-
-		for (c = 'a'; c <= 'z'; c++)
-					{
-								_putchar(c);
-									}
-				_putchar('\n');
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
 }
